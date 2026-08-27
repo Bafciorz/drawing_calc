9429289fcf95cb55fcd92d859ddd0eb42d0b2b18
Bafciorz <jbafia@student.agh.edu.pl>
2026-08-28 01:46:36 +0200

    added javascript website to draw

--- calculator_backend/src/main/java/org/example/calculator_backend/endpoint/controller/CalculatorController.java
+++ calculator_backend/src/main/java/org/example/calculator_backend/endpoint/controller/CalculatorController.java
@@ -5,11 +5,13 @@ import org.example.calculator_backend.endpoint.CalculatorEndpoint;
 import org.example.calculator_backend.model.response.PredictionResponse;
 import org.example.calculator_backend.service.ModelInferenceService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class CalculatorController implements CalculatorEndpoint {
 
     private final ModelInferenceService modelInferenceService;
