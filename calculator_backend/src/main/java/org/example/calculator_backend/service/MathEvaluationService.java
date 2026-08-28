48f3a1672b36bf23adc1ac9a545121a871babb48
Bafciorz <jbafia@student.agh.edu.pl>
2026-08-28 15:43:57 +0200

    added ability to solve equations

--- calculator_backend/src/main/java/org/example/calculator_backend/service/MathEvaluationService.java
+++ calculator_backend/src/main/java/org/example/calculator_backend/service/MathEvaluationService.java
@@ -1,5 +1,6 @@
 package org.example.calculator_backend.service;
 
+import org.example.calculator_backend.model.response.ResultResponse;
 import org.matheclipse.core.eval.ExprEvaluator;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +9,14 @@ public class MathEvaluationService {
 
     public String evaluateExpression(String expression){
         try{
+            String symjaInput = expression;
+
+            if (symjaInput.contains("=")) {
+                symjaInput = symjaInput.replace("=", "==");
+            }
             ExprEvaluator evaluator = new ExprEvaluator();
-            return evaluator.eval(expression).toString();
+            String rawResult = evaluator.eval(symjaInput).toString();
+            return rawResult.replace("==", "=");
         } catch(Exception e){
             return "Cannot evaluate expression";
         }
